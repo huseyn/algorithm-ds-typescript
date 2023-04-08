a10a22488bf3951cc04c5b864fa5b7ae3e0882d4
Huseyn Mikayilov <huseyn.mikayilov@limestonedigital.com>
2023-04-09 01:24:55 +0400

    Create containsDuplicate function

---
+++ array/challanges/4_contains_duplicate.ts
@@ -0,0 +1,23 @@
+/**
+ *  Given an integer array nums, return true if any value appears at least  twice in the array, and return false if every element is distinct.
+ */
+
+/**
+ *  Input: nums = [1,2,3,1]
+    Output: true
+ */
+
+/**
+ *  Input: nums = [1,2,3,4]
+    Output: false
+ */
+
+function containsDuplicate(nums: number[]): boolean {
+  let set = new Set<number>();
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true;
+    set.add(nums[i]);
+  }
+
+  return false;
+}
