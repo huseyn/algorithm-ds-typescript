7b9467953d98a63bd561529b2ecf125647f570fd
Huseyn Mikayilov <huseyn.mikayilov@limestonedigital.com>
2023-04-09 00:27:02 +0400

    Create runningSum function

---
+++ array/challanges/2_running_sum_of_1d_array.ts
@@ -0,0 +1,25 @@
+/**
+ *  Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+    Return the running sum of nums.
+ */
+
+/**
+ *  Input: nums = [1,2,3,4]
+    Output: [1,3,6,10]
+    Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ */
+
+/**
+ *  Input: nums = [1,1,1,1,1]
+    Output: [1,2,3,4,5]
+    Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+ */
+
+function runningSum(nums: number[]): number[] {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] += nums[i - 1];
+  }
+
+  return nums;
+}
