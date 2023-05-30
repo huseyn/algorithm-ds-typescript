e800513a4e96debd4c5e933c58e45c24e93d36fa
Huseyn Mikayilov <huseyn.mikayil95@gmail.com>
2023-05-30 10:09:04 +0400

    Create function for frequency pattern explanation

---
+++ problem_solving_patterns/frequency_counter/index.ts
@@ -0,0 +1,22 @@
+function same(arr1: number[], arr2: number[]) {
+  if (arr1.length !== arr2.length) return false;
+
+  const frequencyCounter1: { [key: string]: number } = {};
+  const frequencyCounter2: { [key: string]: number } = {};
+
+  for (let val of arr1)
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+
+  for (let val of arr2)
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+
+  for (let key in frequencyCounter1) {
+    const parsedKey = parseFloat(key);
+    if (!(parsedKey ** 2 in frequencyCounter2)) return false;
+
+    if (frequencyCounter2[parsedKey ** 2] !== frequencyCounter1[parsedKey])
+      return false;
+  }
+
+  return true;
+}
